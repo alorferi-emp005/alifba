17333e225c18437f41ea33de430ec5218809c43a
Abu Siyam <alorferi.emp005@outlook.com>
2025-04-05 15:37:41 +0600

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
     id("com.google.gms.google-services")
@@ -10,7 +11,7 @@ plugins {
 
 android {
     namespace = "com.alifba.alifba"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.alifba.alifba"
@@ -84,12 +85,12 @@ java {
 
 dependencies {
     // Core Android dependencies
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.activity:activity-compose:1.10.1")
 
     // Use Compose BOM
-    implementation(platform("androidx.compose:compose-bom:2024.10.00"))
+    implementation(platform("androidx.compose:compose-bom:2025.03.01"))
 
     // Compose dependencies without version numbers
     implementation("androidx.compose.ui:ui")
@@ -102,18 +103,18 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata")
 
     // Material Components
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     // Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.8.9")
 
     // Other dependencies
     implementation("com.android.volley:volley:1.2.1")
-    implementation("com.google.firebase:firebase-auth-ktx:23.0.0")
-    implementation("com.google.firebase:firebase-firestore-ktx:25.1.0")
-    implementation("androidx.work:work-runtime-ktx:2.9.1")
-    implementation ("com.google.firebase:firebase-messaging-ktx:24.1.0")
-    implementation("com.google.firebase:firebase-analytics-ktx:21.4.0")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
+    implementation ("com.google.firebase:firebase-messaging-ktx")
+    implementation("com.google.firebase:firebase-analytics-ktx")
 
     // Lottie for animations
     implementation("com.airbnb.android:lottie-compose:4.0.0")
@@ -126,40 +127,41 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     // Firebase BOM
-    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
     implementation("com.google.firebase:firebase-auth")
 
     // Dagger Hilt
     implementation("com.google.dagger:hilt-android:2.51.1")
-    implementation("androidx.test:runner:1.5.2")
+    implementation("androidx.test:runner:1.6.2")
     implementation("androidx.hilt:hilt-common:1.2.0")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // Play Services Auth
-    implementation("com.google.android.gms:play-services-auth:21.2.0")
+    implementation("com.google.android.gms:play-services-auth:21.3.0")
 
     // DataStore
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.4")
 
     // LiveData and Coroutines
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
     // ExoPlayer
     implementation("com.google.android.exoplayer:exoplayer:2.19.1")
 
-    implementation ("androidx.compose.compiler:compiler:1.5.3")
+    //implementation ("androidx.compose.compiler:compiler:1.5.15")
     implementation ("androidx.room:room-runtime:2.6.1")
     implementation ("androidx.room:room-ktx:2.6.1")
     // Testing dependencies
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.03.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    //noinspection GradleDependency
     implementation ("com.onesignal:OneSignal:[5.0.0, 5.1.99]")
     kapt ("androidx.room:room-compiler:2.6.1")
 }
